5e2d0d07ebbaa51fd20b697c58c2182c20933110
mcjiba-5500 <zzj@ithinkcry.cn>
2019-10-29 09:52:03 +0800

    new visit nums template

--- src/main/java/com/zzj/csdnranks/spider/PollService.java
+++ src/main/java/com/zzj/csdnranks/spider/PollService.java
@@ -33,14 +33,16 @@ public class PollService {
                 return numsANDranks;
             }
             Element asideProfile = document.getElementById("asideProfile");
-            Elements fourElements = asideProfile.getElementsByClass("grade-box clearfix");
-            Elements dls = fourElements.get(0).getElementsByTag("dl");
-
-            Element numsElement = dls.get(0);
-            Elements numsDDS = numsElement.getElementsByTag("dd");
+            Elements numElements = asideProfile.getElementsByClass("data-info d-flex item-tiling");
+            Elements num_dls = numElements.get(0).getElementsByTag("dl");
+            Element numsElement = num_dls.get(4);
+            Elements numsDDS = numsElement.getElementsByTag("dl");
             Element numsDl = numsDDS.get(0);
             String numsString = numsDl.attr("title");
             nums = Integer.valueOf(numsString);
+
+            Elements fourElements = asideProfile.getElementsByClass("grade-box clearfix");
+            Elements dls = fourElements.get(0).getElementsByTag("dl");
             Element ranksElement = dls.get(3);
             Elements ranksDDS = ranksElement.getElementsByTag("dl");
             Element ranksDl = ranksDDS.get(0);
